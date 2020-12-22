3a92718c2e773d75f85199fef3feb494adbd9b1c
Yush Kapoor <yushdotkapoor@gmail.com>
2020-12-21 18:48:14 -0800

    Initial Commit

---
+++ Project Mayhem/Project_MayhemApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_MayhemApp.swift
+//  Project Mayhem
+//
+//  Created by Yush Raj Kapoor on 12/21/20.
+//
+
+import SwiftUI
+
+@main
+struct Project_MayhemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
