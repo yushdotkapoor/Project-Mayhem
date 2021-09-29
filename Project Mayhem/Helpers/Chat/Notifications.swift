667002507fb8b85d3f5ca33e237e3dc673f6b8bb
Yush Kapoor <YushDotKapoor@gmail.com>
2021-09-29 13:24:44 -0400

    last update

---
+++ Project Mayhem/Helpers/Chat/Notifications.swift
@@ -0,0 +1,40 @@
+//
+//  Notifications.swift
+//  Project Mayhem
+//
+//  Created by Yush Raj Kapoor on 4/29/21.
+//
+
+
+import UIKit
+
+class PushNotificationSender {
+    func sendPushNotification(to token: String, title: String, body: String) {
+        
+        let urlString = "https://fcm.googleapis.com/fcm/send"
+        let url = NSURL(string: urlString)!
+        let paramString: [String : Any] = ["to" : token,
+                                           "notification" : ["title" : title, "body" : body, "badge":1, "sound":"skibbity.mp3"],
+                                           "data" : ["user" : "test_id"]
+        ]
+        let request = NSMutableURLRequest(url: url as URL)
+        
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject:paramString, options: [.prettyPrinted])
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.setValue(Credentials().key, forHTTPHeaderField: "Authorization")
+        let task =  URLSession.shared.dataTask(with: request as URLRequest)  { (data, response, error) in
+            do {
+                if let jsonData = data {
+                    if let jsonDataDict  = try JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.allowFragments) as? [String: AnyObject] {
+                        print("Received data:\n\(jsonDataDict))")
+                    }
+                }
+            } catch let err as NSError {
+                print(err.debugDescription)
+            }
+        }
+        task.resume()
+    }
+}
+
